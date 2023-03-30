f14ba97b33673ae65e4f4527e67be3130063070d
Elenlith <mishchenkov@list.ru>
2023-03-30 17:19:59 +0300

    make the handler adding data to the DB

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name varchar(755) unique,
+    name varchar(255) unique,
     created_at timestamp
 );
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect
 from dotenv import load_dotenv
 import psycopg2
+from datetime import datetime
+from urllib.parse import urlparse
 
 
 app = Flask(__name__)
@@ -15,6 +17,30 @@ conn = psycopg2.connect(DATABASE_URL)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.post('/urls')
+def add_url():
+    raw_url = request.form.get('url')
+    try:
+        with conn:
+            with conn.cursor() as curs:
+                url = f"{urlparse(raw_url).scheme}://{urlparse(raw_url).netloc}"
+                curs.execute(
+                    """
+                    INSERT INTO urls (name, created_at)
+                    VALUES (%(name)s, %(created_at)s)
+                    RETURNING id;
+                    """,
+                    {'name': url, 'created_at': datetime.now()})
+                flash('Страница успешно добавлена', 'success')
+                return redirect('/')
+    except psycopg2.errors.UniqueViolation:
+        with conn:
+            with conn.cursor() as curs:
+                curs.execute("SELECT id FROM urls WHERE name=(%s);", (url,))
+                flash('Страница уже существует', 'info')
+                return redirect('/')
 
 
 if __name__ == "__main__":
