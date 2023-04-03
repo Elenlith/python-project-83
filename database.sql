b2780f31836ae39e9a7d57234d5879ff8f5e3a62
Elenlith <mishchenkov@list.ru>
2023-04-03 20:41:09 +0300

    make tables in DB using database.sql

--- database.sql
+++ database.sql
@@ -1,13 +1,10 @@
-DROP TABLE IF EXISTS urls CASCADE;
-DROP TABLE IF EXISTS url_checks;
-
-CREATE TABLE urls (
+CREATE TABLE IF NOT EXISTS urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255) UNIQUE,
     created_at timestamp
 );
 
-CREATE TABLE url_checks (
+CREATE TABLE IF NOT EXISTS url_checks (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url_id bigint REFERENCES urls(id),
     status_code integer,
